5efa8ed133cf7818fd97a4a7fecbb13630d43acd
Richard Chung <80141657+gregwoodsfrontier@users.noreply.github.com>
2021-05-30 21:53:51 +0800

    test1

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,10 +1,21 @@
 export const BG = 'bg';
 export const AAVEGOTCHI_LOGO = 'aavegotchi_logo';
+export const GALAXY = 'galaxy';
+export const LOGO = 'logo';
+export const SUSHIVADER = 'sushiVader';
+
 
 // Icons
 export const FULLSCREEN = 'fullscreen';
 export const LEFT_CHEVRON = 'left_chevron';
 export const RIGHT_CHEVRON = 'right_chevron';
+export const GEM = 'gem';
+export const EXPLORE = 'explore';
+export const SUSHI1 = 'sushi1';
+export const SUSHI2 = 'sushi2';
+export const SUSHI3 = 'sushi3';
+export const PBULLET = 'pbullet';
+export const EBULLET = 'ebullet';
 
 // Sounds
 export const BOOP = 'boop';
@@ -15,3 +26,7 @@ export const PORTAL_OPENED = 'portal_opened';
 export const SEND = 'send';
 export const SENDING = 'sending';
 export const SUCCESS = 'success';
+export const SHOOT = 'shoot';
+export const KABOOM = 'kaboom';
+export const INVADERKILLED = 'invaderkilled';
+export const ESCAPETHEFUD = 'escapethefud';
